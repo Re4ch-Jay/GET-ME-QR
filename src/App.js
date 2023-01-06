a5d9ab2c2829c0f2ec85a6a9c28c58d5fa5ab03d
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-01-06 22:34:54 +0700

    route & 404 page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 import Home from './pages/Home'
+import Notfound from "./pages/Notfound";
 function App() {
 
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar/> 	
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Notfound />} />
         </Routes>
         <Footer/>
       </div>
