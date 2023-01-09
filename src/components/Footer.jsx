dd95832caae7196d2347a495b53a95a28ff2b025
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-01-09 11:32:21 +0700

    footer link fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     <footer className="p-4 bg-white sm:p-6 bg-gray-900">
         <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
-                <a href="https://flowbite.com/" className="flex items-center">
+                <a href="#" className="flex items-center">
                     <img src="images/logo.png" className="h-8 mr-3" alt="Get Me QR Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Get Me QR</span>
                 </a>
@@ -15,10 +15,10 @@ function Footer() {
                     <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase text-white">Resources</h2>
                     <ul className="text-gray-600 text-gray-400">
                         <li className="mb-4">
-                            <a href="https://flowbite.com/" className="hover:underline">Get Me QR</a>
+                            <a href="#" className="hover:underline">Get Me QR</a>
                         </li>
                         <li>
-                            <a href="https://tailwindcss.com/" className="hover:underline">Tailwind CSS</a>
+                            <a href="#" className="hover:underline">Tailwind CSS</a>
                         </li>
                     </ul>
                 </div>
@@ -26,10 +26,10 @@ function Footer() {
                     <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase text-white">Follow us</h2>
                     <ul className="text-gray-600 text-gray-400">
                         <li className="mb-4">
-                            <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
+                            <a href="#" className="hover:underline ">Github</a>
                         </li>
                         <li>
-                            <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
+                            <a href="#" className="hover:underline">Discord</a>
                         </li>
                     </ul>
                 </div>
@@ -48,7 +48,7 @@ function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto border-gray-700 lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center text-gray-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Get Me QR™</a>. All Rights Reserved.
+            <span className="text-sm text-gray-500 sm:text-center text-gray-400">© 2023 <a href="#" className="hover:underline">Get Me QR™</a>. All Rights Reserved.
             </span>
             <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <a href="#" className="text-gray-500 hover:text-gray-900 hover:text-white">
