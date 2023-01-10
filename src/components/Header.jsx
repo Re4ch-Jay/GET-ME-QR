3a064ed0852f78664fc866bc44f6cdc424e23806
Phat Panhareach <111555521+Re4ch-Jay@users.noreply.github.com>
2023-01-10 12:56:30 +0700

    ...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Header() {
   return (
     <div className='border-gray-200 px-2 sm:px-4 py-2.5 rounded bg-black fixed w-full z-20 top-0 left-0'>
-        <h3 className='text-white text-center'>Support Ukraine &#127482;&#127462;</h3>
-        <h6 className='text-[#fbbf24] text-center font-bold underline cursor-pointer'>Help Provide Humanitarian Aid to Ukraine.</h6>
+        <h3 className='text-white text-center'>Black Lives matter &#128170;</h3>
+        <h6 className='text-[#fbbf24] text-center font-bold underline cursor-pointer'>We all bleed the same color</h6>
     </div>
   )
 }
